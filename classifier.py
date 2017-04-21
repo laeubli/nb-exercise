6a1bad3b755397b123cf0e2da1198e8de22a319a
Samuel Läubli <laeubli@ifi.uzh.ch>
2017-04-21 19:04:16 +0200

    add todo for the purpose of this exercise

--- classifier.py
+++ classifier.py
@@ -28,13 +28,8 @@ class NBClassifier:
                 for word, frequency in self._get_word_freqs(doc).items():
                     self._word_freqs[current_class][word] += frequency
                     self._vocabulary.add(word)
-        for current_class in self._classes:
-            num_docs_of_current_class = len(documents[current_class])
-            self._logprior[current_class] = log10(num_docs_of_current_class / num_docs_of_any_class)
-            num_words = sum(self._word_freqs[current_class].values())
-            num_words += len(self.vocabulary()) # add-1 smoothing
-            for word in self.vocabulary():
-                self._loglikelihood[current_class][word] = log10((self._word_freqs[current_class][word] + 1) / num_words)
+        #TODO: Calculate self._logprior and self._loglikelihood for all classes
+        #      and words. Don't forget about smoothing.
 
     def classify(self, document):
         """
